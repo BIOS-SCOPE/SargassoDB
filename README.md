ee96efe85efab3b763353f39423c5fda0cb0563e
redbluewater <klongnecker@whoi.edu>
2026-03-31 15:51:33 -0700

    have the basics working

--- README.md
+++ README.md
@@ -4,6 +4,17 @@
 Actually this is useful as I can actually get this database to run which I could not do with the newer code.\
 Keep this and move on from here.
 
+Some basic steps:
+```bash
+venv\Scripts\activate
+pip install -r requirements.txt (only once)
+set SARGASSODB_CONFIG_FILE=%cd%\sargassodb\config.json (not sure if this is used here, but cannot hurt)
+python populate_db.py (will take a while)
+
+```
+
+Will produce a database in sql_app.db
+
 ## updated 13 March 2026 (Krista)
 This is deprecated, use bioscope-portal instead.
 
--- populate_db.py
+++ populate_db.py
@@ -3,8 +3,10 @@ from sqlalchemy import create_engine, inspect, MetaData
 from sqlalchemy.orm import sessionmaker, Session
 from datetime import datetime, time
 import models
-from tqdm import tqdm
-import pdb
+from tqdm import tqdm #will show progress bar as you move through a loop
+import pdb #use with pdb.set_trace()
+
+#KL working copy
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
@@ -109,9 +111,9 @@ def load_asv_samples():
 
 
 def load_rel_abundances():
-	#KL working here, this fill is too big for GitHub so have to get it from elsewhere
-    #df = pd.read_csv('test_data/mini_test_asv_rel_abundances.csv')
-    df = pd.read_csv('test_data/bats49month_fulldata_relativeabundance_May_28_2021.csv')
+    df = pd.read_csv('test_data/mini_test_asv_rel_abundances.csv')
+    #KL working here, this fill is too big for GitHub so have to get it from elsewhere
+    #df = pd.read_csv('test_data/bats49month_fulldata_relativeabundance_May_28_2021.csv')
 
     #work out the sample ids first to only have to do it once
     seq_mapping = {}
@@ -130,7 +132,11 @@ def load_rel_abundances():
         else:
             print(f'Sample name {sample_name} does not appear to have a sample associated with it')
 
+    #KL issue with the next line error is: id_vars are not in the DataFrame: ['short_name']
+    #pdb.set_trace()
+    #based on what I see in the mini file, just add short_name to the first column (the file from github has nothing labeling the first column)
     long_format = pd.melt(df, id_vars=['short_name', 'seqs'], var_name= 'sample_name', value_vars=df.columns[2:])
+    
     session = Session()
     counter = 0
     print('Loading ASV relative abundances')
